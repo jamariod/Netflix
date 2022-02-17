55db8893fc2d1ad456c9aa12b9462ecc9942cf97
jamariod <j25ghs99@aol.com>
2022-02-17 12:15:30 -0500

    Initial Commit

---
+++ Netflix/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarViewController.swift
+//  Netflix
+//
+//  Created by Jamario Davis on 2/17/22.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
